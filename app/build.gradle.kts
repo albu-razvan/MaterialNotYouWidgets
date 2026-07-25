8e31108d16ca776187aa4276edaa184d9b594ace
Razvan Albu <razvan-albu@hotmail.com>
2026-07-25 15:41:46 +0300

    chore: bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.razvanalbu.material.not.you.widgets"
         minSdk = 31
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     signingConfigs {
